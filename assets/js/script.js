65216527dd38b0ed2493ae533b8177e6ab9755f5
Vlad Saftoiu <vlad.saftoiu@live.com>
2014-12-21 20:12:44 +0100

    test ng-include

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+(function(angular) {
+  'use strict';
+
+  angular.module('ctug', ['ngAnimate'])
+		.controller('section2Controller', ['$scope', function($scope) {
+			$scope.hello = "hello";
+	  }]);
+	  
+})(window.angular);
